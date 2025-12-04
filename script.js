054c5856c867539384be881b95714871cea0a03e
Patience Condell <condellp@gmail.com>
2025-12-04 02:51:34 -0600

    Modified some changes

--- script.js
+++ script.js
@@ -1,24 +1,42 @@
-const ageInput = datepicker('.age-calculate', { id: 1 });
-const button = document.getElementById('calculate-button');
-button.addEventListener('click', function (e) {
-    e.preventDefault();
-    const ageValue = document.querySelector('.age-calculate').value;
-    const result = document.getElementById('result');
-    if (!ageValue) {
-        result.innerText = 'Заполните дату рождения!';
-        return;
+let calculate_btn = document.getElementById('calculate_btn');
+let calculate_age = document.getElementById('calculate_age');
+
+calculate_btn.addEventListener('click',function(){
+
+    let birth_date = document.getElementById('birth_date').value;
+    birth_date = new Date(birth_date);
+    //alert(birth_date)
+    let todayDate = new Date();
+    //alert(todayDate)
+    totalAge = Date.now() - birth_date.getTime();
+    ageYear = new Date(totalAge);
+    ageYear = Math.abs(ageYear.getFullYear() - 1970);
+
+        function ageMonth(){
+            if(todayDate.getMonth() >= birth_date.getMonth()){
+                
+                if(todayDate.getDate() >= birth_date.getDate()){
+                    return todayDate.getMonth() - birth_date.getMonth();
+                }else{
+                    if ((todayDate.getMonth() - 1) >= birth_date.getMonth()){
+                        return (todayDate.getMonth() -1) - birth_date.getMonth();
+                }else{
+                    return ((todayDate.getMonth() - 1) + 12) - birth_date.getMonth();
+                }
+            }
+        }
     }
-    const { DateTime } = luxon;
-    const birthDate = DateTime.fromISO(ageValue);
-    if (!birthDate.isValid) {
-        result.innerText = 'Неверный формат даты! Используйте формат YYYY-MM-DD.';
-        return;
+
+        function ageDay(){
+            if(todayDate.getDate() > birth_date.getDate()){
+                return todayDate.getDate() - birth_date.getDate();
+            }else if ( todayDate.getDate() == birth_date.getDate()){
+                return todayDate.getDate() - birth_date.getDate();
+            }else{
+                let calDate = new Date(birth_date.getFullYear(), birth_date.getMonth(),0);
+                return (todayDate.getDate() + calDate.getDate()) - birth_date.getDate();
+            }
     }
-    const now = DateTime.now();
-    if (birthDate > now) {
-        result.innerText = 'Дата рождения не может быть больше текущей даты!';
-        return;
-    }
-    const diff = now.diff(birthDate, ['years', 'months']).toObject();
-    result.innerText = `Вам ${Math.floor(diff.years)} лет и ${Math.floor(diff.months)} месяцев.`;
-});
+
+    calculate_age.innerHTML = "Your Age " + ageYear + " Years " + ageMonth() + "Months" + (" ") + ageDay() + "Days"
+    })
